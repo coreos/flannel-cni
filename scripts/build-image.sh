952f322b465c744ce1701216df2125cc8f866ce0
halfcrazy <hackzhuyan@gmail.com>
2019-11-08 10:28:39 +0800

    Update build-image.sh
    Upgrade cni-plugin to latest version, since portmap has a bug. 
    https://github.com/containernetworking/plugins/pull/269

--- scripts/build-image.sh
+++ scripts/build-image.sh
@@ -4,7 +4,7 @@ CURL="curl -sSf"
 FLANNEL_CNI_ROOT=$(git rev-parse --show-toplevel)
 IMAGE_NAME=quay.io/coreos/flannel-cni
 VERSION=$($FLANNEL_CNI_ROOT/scripts/git-version)
-CNI_VERSION="v0.6.0"
+CNI_VERSION="v0.8.2"
 
 mkdir -p dist
 $CURL -L --retry 5 https://github.com/containernetworking/cni/releases/download/$CNI_VERSION/cni-amd64-$CNI_VERSION.tgz | tar -xz -C dist/
